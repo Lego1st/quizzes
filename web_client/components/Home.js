68a7e844359c74610fdc4a71dd36d9f10597558f
Trung Nghia <nguyentrungnghia1812@gmail.com>
2018-11-13 00:04:04 +0700

    QuizItem component update

--- web_client/components/Home.js
+++ web_client/components/Home.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import QuizItem from "./QuizItem"
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      quiz_pending_list : [
+        {
+          title: "Get started wit Quizzes",
+          description: "Brief description 1 here...",
+          category: "Test_Category",
+          rated: 3
+        },
+        {
+          title: "How Quizzes rank you ?",
+          description: "Brief description 2 here...",
+          category: "Test_Category",
+          rated: 2
+        }
+      ],
+      top_quiz_list: [
+        {
+          title: "Quiz 1",
+          description: "Brief description 1 here...",
+          category: "Math",
+          rated: 3
+        },
+        {
+          title: "Quiz 2",
+          description: "Brief description 2 here...",
+          category: "Kid",
+          rated: 2
+        },
+        {
+          title: "Quiz 3",
+          description: "Brief description 3 here...",
+          category: "Fun",
+          rated: 1
+        }
+      ]
+    }
+  }
+
+  renderQuizList(quiz_list) {
+    var quizzes = [];
+    for (var i = 0; i < quiz_list.length; i++) {
+      quizzes.push(<QuizItem info={quiz_list[i]}/>)
+    }
+    return quizzes;
   }
 
   render() {
+ 
     return (
       <div className="container" id="home-page">
         <div className="row">
@@ -24,56 +70,17 @@ class Home extends Component {
 
             <div id="qz_pending_list">
               <div className="qz_list_title">
-                Quiz for you
-          </div>
-              <div className="qz_quiz_item">
-                <div className="qz_quiz_title">Get started wit Quizzes</div>
-                <div className="qz_quiz_desc">Brief description here...</div>
-                <div className="qz_quiz_rating">
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                </div>
+                Quiz for you 
               </div>
-              <div className="qz_quiz_item">
-                <div className="qz_quiz_title">How Quizzes rank you ?</div>
-                <div className="qz_quiz_desc">Brief description here...</div>
-                <div className="qz_quiz_rating">
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                </div>
-              </div>
+              {this.renderQuizList(this.state.quiz_pending_list)}
+            
             </div>
 
             <div id="qz_pending_list">
               <div className="qz_list_title">
                 Top quiz
-          </div>
-              <div className="qz_quiz_item">
-                <div className="qz_quiz_title">Quiz 1</div>
-                <div className="qz_quiz_desc">Brief description here...</div>
-                <div className="qz_quiz_rating">
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                </div>
               </div>
-              <div className="qz_quiz_item">
-                <div className="qz_quiz_title">Quiz 2</div>
-                <div className="qz_quiz_desc">Brief description here...</div>
-                <div className="qz_quiz_rating">
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                </div>
-              </div>
-              <div className="qz_quiz_item">
-                <div className="qz_quiz_title">Quiz 3</div>
-                <div className="qz_quiz_desc">Brief description here...</div>
-                <div className="qz_quiz_rating">
-                  <span className="qz_rating"></span>
-                  <span className="qz_rating"></span>
-                </div>
-              </div>
+              {this.renderQuizList(this.state.top_quiz_list)}
             </div>
 
           </div>
---
+++ web_client/components/QuizItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class QuizItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderRating() {
+    var rate = [];
+    for(var i = 0; i < this.props.info.rated; i++)
+      rate.push(<span className="qz_rating"></span>)
+    return rate;
+  }
+
+  render() {
+    return (
+      <div className="qz_quiz_item">
+        <div className="qz_quiz_title">{this.props.info.title}</div>
+        <div className="qz_quiz_desc">{this.props.info.description}</div>
+        <div className="qz_quiz_rating">
+          {this.renderRating()}
+        </div>
+        <div className="qz_quiz_cate">{this.props.info.category}</div>
+      </div>  
+    );
+  }
+}
+
+export default QuizItem;
