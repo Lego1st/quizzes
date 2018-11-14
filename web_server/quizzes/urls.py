44504c5c567037dae9bb708220ac76c1cf216d15
Bùi Mạnh Thắng <buimanhthangnt@gmail.com>
2018-11-14 09:48:45 +0700

    update url webserver

--- web_server/quizzes/urls.py
+++ web_server/quizzes/urls.py
@@ -1,17 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('api/profile/statistic', views.PSListCreate.as_view()),
     path('api/profile/general-info', views.UserListCreate.as_view()),
-    path('profile/', views.index),
-    path('category/', views.index),
-    path('myquizzes/', views.index),
-    path('favorite/', views.index),
-    path('answered/', views.index),
-    path('leaderboard/', views.index),
-    path('quizapproval/', views.index),
-    path('quiz/', views.index),
-    path('home/',views.index),
-    path('', views.index),
+    re_path(r'^.*$', views.index),
 ]
