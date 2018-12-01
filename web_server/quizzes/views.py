8df799295ee6443a6b072186e28cf3b021615e91
Cloud <minhkjl3@gmail.com>
2018-12-01 17:13:11 +0700

    add author only permission, hasntdonequiz permission, quiz result api

--- web_server/quizzes/views.py
+++ web_server/quizzes/views.py
@@ -14,22 +14,60 @@ import pandas
 def index(request):
     return render(request, 'index.html')
 
-#Pagination class
+# Pagination classes
 class StandardPaginationResult(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-## Quiz and Question api
+# Permission classes
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of the quiz.
+        return obj.author == request.user
+
+class HasntDoneQuizOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow user to do each quiz only once
+    """
+    message = 'User has done this quiz.'
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        quiz = obj
+        return UserSubmission.objects.filter(quiz=quiz, user=user).count() == 0
+
+# Quiz and Question APIs
 class QuizQuestionDetail(generics.RetrieveAPIView):
 
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated,
+                        HasntDoneQuizOnly,)
     queryset = Quiz.objects.all()
     serializer_class = QuizQuestionReadOnlySerializer
 
+class QuizResult(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSubmissionSerializer
+
+    def get_queryset(self):
+        quiz_id = self.kwargs['pk']
+        quiz = Quiz.objects.get(pk=quiz_id)
+        user = self.request.user
+        return UserSubmission.objects.filter(quiz=quiz, user=user)
+
 class FullQuizDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                        IsAuthorOrReadOnly,)
     queryset = Quiz.objects.all()
     serializer_class = FullQuizSerializer
 
