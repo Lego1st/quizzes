d425a0894a59bec841cea6a4d73e08a22a951947
thangbm <buimanhthangnt@gmail.com>
2018-12-01 15:23:29 +0700

    quiz approval front end

--- web_server/quizzes/urls.py
+++ web_server/quizzes/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/recent_quiz/', views.RecentQuiz.as_view()),
     path('api/posted_quiz/', views.PostedQuiz.as_view()),
     path('api/submit_quiz/', views.UserSubmit.as_view()),
+    path('api/pending_quiz/', views.PendingQuiz.as_view()),
     path('api/upfile/',views.upload_file_quiz),
     path('api/search/<search_text>/', views.SearchQuiz.as_view()),
     re_path(r'^.*$', views.index),
