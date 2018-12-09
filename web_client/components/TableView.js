f0763ef7e23f24e702a38089fab6f1451fe135f9
thangbm <buimanhthangnt@gmail.com>
2018-12-09 13:47:15 +0700

    message for empty category list

--- web_client/components/TableView.js
+++ web_client/components/TableView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Link } from 'react-router-dom';
 // use default loading spinners
 import ReactRefreshInfiniteTableView from '../lib/ReactRefreshInfiniteTableView'
 import QuizItem from './QuizItem'
@@ -17,7 +17,20 @@ export default class TableView extends ReactRefreshInfiniteTableView {
     ));
     return (
       <div id="qz_pending_list" onScroll={this.viewDidScroll}>
-        {quizzes}
+        {quizzes.length == 0 ? 
+          <div style={{ textAlign: 'center' }}>
+            <h3>It looks like we currently do not have any post yet ^^</h3>
+
+            <h4>Click
+                <Link to="/addquiz">
+                <i className="fas fa-plus-circle" style={{ margin: '1%' }}></i>
+              </Link>
+              to add one!
+                  </h4>
+          </div>
+          :
+          quizzes
+        }
       </div>
     )
   }
